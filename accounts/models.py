8c40a1fbeb4ef90d9a574fa855c252ae999c1e8d
Ezra Yendau <5201040538@st.aamusted.edu.gh>
2024-06-09 20:38:56 +0000

    changes user is_active sate

--- accounts/models.py
+++ accounts/models.py
@@ -38,7 +38,7 @@ class User(AbstractUser):
     gender = models.CharField(
         max_length=10, choices=Gender.choices, default=Gender.OTHER
     )
-    is_staff =models.BooleanField(default=False)
+    is_staff =models.BooleanField(default=True)
     is_active = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
